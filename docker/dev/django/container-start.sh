34a9ba406e4f0983636c8c8b620a537bfe425ed0
Tashi <tashidhondup55@gmail.com>
2025-04-07 10:04:59 +0530

    fix the migration issue after EBS volume wipeout

--- docker/dev/django/container-start.sh
+++ docker/dev/django/container-start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 python manage.py migrate --noinput  && \
 python manage.py collectstatic --noinput  && \
-python manage.py seed && \
+if [ "$SEED_ON_STARTUP" = "true" ]; then
+  python manage.py seed
+fi && \
 uwsgi --ini /code/docker/dev/django/uwsgi.ini
